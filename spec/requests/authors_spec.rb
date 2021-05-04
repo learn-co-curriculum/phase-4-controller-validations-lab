e116cca92e1bf0244eb6d9ff1632bc826493ec90
ihollander <ianholla@gmail.com>
2021-05-04 13:02:59 -0400

    Updated tests

--- spec/requests/authors_spec.rb
+++ spec/requests/authors_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe "Authors", type: :request do
       let!(:author_params) { { name: 'S. Bro', email: 'bro@sbahj.info' } }
   
       it 'creates a new author' do
-        expect { post '/authors', params: author_params }.to change(author, :count).by(1)
+        expect { post '/authors', params: author_params }.to change(Author, :count).by(1)
       end
   
       it 'returns the author data' do
@@ -34,17 +34,14 @@ RSpec.describe "Authors", type: :request do
       let!(:author_params) { { email: 'jeff@sbahj.info' } }
   
       it 'does not creates a new author' do
-        expect { post '/authors', params: author_params }.to change(author, :count).by(0)
+        expect { post '/authors', params: author_params }.to change(Author, :count).by(0)
       end
   
       it 'returns the error messages' do
         post '/authors', params: author_params
   
         expect(response.body).to include_json({
-          errors: {
-            name: "can't be blank",
-            email: "has already been taken"
-          }
+          errors: a_kind_of(Hash)
         })
       end
   
