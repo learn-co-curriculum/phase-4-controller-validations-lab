58a6424ca031cbf3cb4f8a224a0e2f1b417b0b9c
ihollander <ianholla@gmail.com>
2021-05-30 08:46:18 -0400

    Refactor solution for exception handling

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,18 +2,14 @@ class AuthorsController < ApplicationController
   
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
-    if author.valid?
-      render json: author, status: :created
-    else
-      render json: { errors: author.errors }, status: :unprocessable_entity
-    end
+    author = Author.create!(author_params)
+    render json: author, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,19 +2,15 @@ class PostsController < ApplicationController
 
   def show
     post = Post.find(params[:id])
-    
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-    if post.valid?
-      render json: post
-    else
-      render json: { errors: post.errors }, status: :unprocessable_entity
-    end
+    post.update!(post_params)
+    render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
