2af27f4287602ac99cc09a98abb7709d4a642539
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-06-22 22:11:08 -0700

    controller validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,9 +9,11 @@ class PostsController < ApplicationController
   def update
     post = Post.find(params[:id])
 
-    post.update(post_params)
-
-    render json: post
+    if post.update(post_params)
+      render json: post
+    else
+      render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
 end
